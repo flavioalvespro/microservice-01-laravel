d5403ea3476a942cb456ba8b059f5afaee2897ac
Flavio Alves <jose.flavio@jsl.com.br>
2021-10-18 16:23:40 -0300

    consume external service http

--- app/Services/EvaluationService.php
+++ app/Services/EvaluationService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services;
 
-use App\Services\Traits\ConsumeExternalService;
+use Flavioalvespro\MicroservicesCommon\Services\Traits\ConsumeExternalService;
 
 class EvaluationService
 {
